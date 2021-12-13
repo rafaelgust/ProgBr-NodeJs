2ccfb4009189c28c0679351311ccf162163a03fa
rafaelgust <rafael.appdevs@gmail.com>
2021-12-13 14:56:13 -0300

    update result

--- processArg.js
+++ processArg.js
@@ -1,12 +1,17 @@
+//node processArg 50 30
 let args = process.argv.slice(2);
-console.log(args);
-
-
 let a = args[0];
 let b = args[1];
-
+// [ '50', '30' ]
 const soma = (x,y) => x + y;
+let resultConc = soma(a,b);
+console.log(resultConc);
+//5030
+//arg vem como string e a soma termina como uma concatenação
 
-let result = soma(a,b);
+let c = Number(args[0]);
+let d = Number(args[1]);
 
-console.log(result);
+let resultSom = soma(c,d);
+console.log(resultSom);
+//80
