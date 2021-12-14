86ba484f74de4f0861cc3fa457195f2d0b25571d
rafaelgust <rafael.appdevs@gmail.com>
2021-12-14 16:57:26 -0300

    change resize

--- resizeImg/app.js
+++ resizeImg/app.js
@@ -1,13 +1,36 @@
 const sharp = require('sharp');
+const compress_images = require('compress-images');
 
 let path = process.argv[2]; // pegar img
 let width = Number(process.argv[3]); //tamanho
 
 function resize(path, width){
-    sharp(path).resize({ width: width })
-        .toFile('./temp/output_resize.jpg', (err) => {
-        err ? console.log(err) : console.log('Imagem Redimensionada');
-    });
+    
+    sharp(path)
+        .resize({width: width})
+        .toFile('./outputSize.png', (err, info) => {
+            err ? console.log(err) : console.log(info);
+        });
 }
 
 resize(path, width);
+
+function compress(pathInput, outputPath){
+    compress_images(pathInput, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+        function (error, completed, statistic) {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+        }
+    );
+}
+
+//compress(path, "./compressed");
+
+
