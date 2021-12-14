20ee430624d24b7640582f240c992706f29b8e9c
rafaelgust <rafael.appdevs@gmail.com>
2021-12-14 16:07:18 -0300

    module and resizeImg

---
+++ resizeImg/app.js
@@ -0,0 +1,12 @@
+const sharp = require('sharp');
+
+let path = process.argv[2]; // pegar img
+let width = Number(process.argv[3]); //tamanho
+
+function resize(path, width){
+    sharp(path).resize({width: width}).toFile('./temp/output_resize.jpg', (err) => {
+        err ? console.log(err) :console.log('Imagem Redimensionada');
+    });
+}
+
+resize(path, width);
