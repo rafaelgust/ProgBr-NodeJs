2f31d3a032b0ab99da381100bd5f81d0b82160ec
rafaelgust <rafael.appdevs@gmail.com>
2021-12-15 15:02:20 -0300

    page server work

--- pagesServer/index.js
+++ pagesServer/index.js
@@ -2,20 +2,22 @@ const http = require('http'), url = require('url'), fs = require('fs');
 
 http.createServer((req, res) => {
     let path = url.parse(req.url).pathname;
-    let fileName = `.${path}`;
- 
     if(path === '' || path === '/'){
-        fileName = 'index.html';
+        path = '/index.html';
     }
+    let fileName = `.${path}`;
 
     fs.readFile(fileName, (err, data) => {
         if(err){
-            res.writeHead(404, {'Content-type': 'text/html'});
+            res.writeHead(404, {'Content-type': 'text/html; charset=utf-8'});
+            res.end("<h1>Página não foi Encontrada !!!</h1>");
         }else{
-            res.writeHead(200, {'Content-type': 'text/html'});
+            res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'});
+            res.write(data);
+            res.end();
         }
     });
 
 }).listen(3002, (err)=>{
-
+    err ? console.log(err + ' Não foi possível criar o servidor') : console.log('Servidor criado na porta 3002');
 });
