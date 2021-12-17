5b8fb867922880602ccd742c80c81fd337730983
rafaelgust <rafael.appdevs@gmail.com>
2021-12-17 15:19:17 -0300

    express client

--- express/index.js
+++ express/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client')));
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server Running on Port: ${PORT}`);
