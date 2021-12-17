906a9f224e070525057fab741c59dafe29039bd4
rafaelgust <rafael.appdevs@gmail.com>
2021-12-17 16:11:17 -0300

    all methods with index.html buttons

--- express/index.js
+++ express/index.js
@@ -3,11 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
-app.use(express.static(path.join(__dirname, 'client')));
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+app.use(express.static(path.join(__dirname, './client')));
+app.get("/methods", (req, res) => {
+    res.send('<h1>get</h1>');
+});
+app.post("/methods", (req, res) => {
+	res.send('<h1>post</h1>');
+});
+app.put("/methods", (req, res) => {
+	res.send('<h1>put</h1>');
+});
+app.delete("/methods", (req, res) => {
+	res.send('<h1>delete</h1>');
 });
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port: ${PORT}`);
-});
+})
