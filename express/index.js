55719a2f1668225874c91a4a37aaf7e3e9760502
rafaelgust <rafael.appdevs@gmail.com>
2021-12-17 15:46:27 -0300

    express get

--- express/index.js
+++ express/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const PORT = 5000;
 
 app.use(express.static(path.join(__dirname, 'client')));
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
 
-const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server Running on Port: ${PORT}`);
 });
